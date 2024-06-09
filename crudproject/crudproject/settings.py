2448ad94a175bed4b3912d82ee0a5c28363f0d3c
Vicky Kumar <imvickykumar999@gmail.com>
2024-06-09 18:53:49 +0530

    media path

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -116,16 +116,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# default static files settings for PythonAnywhere.
+# see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
+
+# Define the URL where media files will be served from
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# default static files settings for PythonAnywhere.
-# see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-
-MEDIA_ROOT = '/crudproject/media'
-MEDIA_URL = '/media/'
-
-STATIC_ROOT = '/crudproject/static'
