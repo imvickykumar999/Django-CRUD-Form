f0ee93f1dcf24e51550293d29edf72d503d04962
Vicky Kumar <imvickykumar999@gmail.com>
2024-06-09 17:51:53 +0530

    zip -r myzipfile my_folder_name

---
+++ myzipfile/home/crudproject/crudapp/forms.py
@@ -0,0 +1,8 @@
+
+from django import forms
+from .models import GeeksModel
+
+class GeeksForm(forms.ModelForm):
+    class Meta:
+        model = GeeksModel
+        fields = ['title', 'description', 'foto']
